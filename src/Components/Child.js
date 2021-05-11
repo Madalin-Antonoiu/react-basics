3d128d7e43ed9d7fecd7ed17a135cfbe30338415
madalin@heron-web.com <madalin@heron-web.com>
2021-05-11 12:39:58 +0100

    One component with two types, comparing state between them.

--- src/Components/Child.js
+++ src/Components/Child.js
@@ -1,14 +1,17 @@
 import React from "react"
 
 class Child extends React.Component {
-    handleChange = (e) => this.props.onChange(e.target.value);
+    handleChange = (e) => this.props.onChange(e.target.value, this.props.type);
 
     render() {
         const value = this.props.value;
         const onChange = this.handleChange;
 
-        return <input value={value} onChange={onChange} />
+        return <>
+            <input value={value} onChange={onChange} />
+            <button onClick={() => console.log(this.props)}>Debug</button>
+        </>
     }
 }
 
-export default Child
+export default Child;
--- src/Components/Parent.js
+++ src/Components/Parent.js
@@ -2,23 +2,32 @@ import React from "react"
 import Child from "./Child"
 
 class Parent extends React.Component {
-    state = {value: ''};
+    state = {value1: '', value2: ''};
 
-    handleChange = (value) => this.setState({value});
+    handleChange = (value, type) => {
+        type === "first" ? this.setState({value1: value}) : this.setState({value2: value});
+
+        if (value === this.state.value2) {
+            console.log("YAY, equal")
+        }
+
+    }
 
     render() {
-        const value = this.state.value;
+        const value1 = this.state.value1;
+        const value2 = this.state.value2;
         const onChange = this.handleChange;
 
         return <>
             <Child
-                value={value}
+                type="first"
+                value={value1}
                 onChange={onChange} />
 
             <Child
-                value={value}
+                type="second"
+                value={value2}
                 onChange={onChange} />
-
         </>
     }
 }
